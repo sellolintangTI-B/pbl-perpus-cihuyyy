5df9c8611e749905ecb40231b0271c1cfb16ae21
farrelmaahiraTI-B <farrel.maahira.agraprana.nugraha.tik24@stu.pnj.ac.id>
2025-10-19 14:59:45 +0700

    merging

--- app/components/layout/Default.php
+++ app/components/layout/Default.php
@@ -27,6 +27,7 @@
     <!-- Error Messages -->
     <?php
         $error = ResponseHandler::getResponse();
+        var_dump($error);
         if(!empty($error)) {
     ?>
         <div class="fixed top-0 left-0 right-0 z-50 flex justify-center p-4 alert-slide">
