d1c58b1970599f4bbdcba0606e269f4e19c5cf08
Nugroho <nugrohonug066@gmail.com>
2025-11-06 14:15:38 +0700

    benerin captcha

--- app/utils/Captcha.php
+++ app/utils/Captcha.php
@@ -19,7 +19,24 @@ class Captcha {
         $code = implode($code);
         $_SESSION['captcha'] = $code;
 
-        return $code;
-
+        $width = 200;
+        $height = 50;
+        $image = imagecreatetruecolor($width, $height);
+        
+        $bg_color = imagecolorallocate($image, 11, 63, 120);
+        $text_color = imagecolorallocate($image, 255, 255, 255);
+        
+        imagefill($image, 0, 0, $bg_color);
+        
+        imagestring($image, 5, 50, 15, $code, $text_color);
+        
+        header('Content-Type: image/jpeg');
+        header('Cache-Control: no-cache, no-store, must-revalidate');
+        header('Pragma: no-cache');
+        header('Expires: 0');
+        
+        imagejpeg($image);
+        imagedestroy($image);
+        exit;
     }
 }
