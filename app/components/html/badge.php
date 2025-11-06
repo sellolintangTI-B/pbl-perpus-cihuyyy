4257507ce8bde5ddc1782202b7bb4bcf13ff56c5
Nugroho <nugrohonug066@gmail.com>
2025-11-06 13:04:59 +0700

    fixing fe

--- app/components/html/badge.php
+++ app/components/html/badge.php
@@ -1,3 +1,3 @@
-<div class="px-3 py-2 w-24 rounded-full flex items-center justify-center <?=$active?"bg-secondary/20 text-secondary":"bg-red/20 text-red"?>">
+<div class="px-2 py-1 w-24 rounded-full flex items-center justify-center <?=$active?"bg-secondary/20 text-secondary":"bg-red/20 text-red"?>">
     &bull; <?=$label?>
 </div>
