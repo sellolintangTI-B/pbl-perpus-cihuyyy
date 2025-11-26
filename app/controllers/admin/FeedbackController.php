4b0a14dba745c3e54990195f3f017dfc2d3ad08b
Nugroho <nugrohonug066@gmail.com>
2025-11-26 13:10:40 +0700

    filter di feedback

--- app/controllers/admin/FeedbackController.php
+++ app/controllers/admin/FeedbackController.php
@@ -6,6 +6,7 @@ use App\Core\Controller;
 use App\Core\ResponseHandler;
 use app\error\CustomException;
 use App\Models\Feedback;
+use App\Models\Room;
 use Carbon\Carbon;
 
 class FeedbackController extends Controller
@@ -21,9 +22,12 @@ class FeedbackController extends Controller
             if (isset($_GET['date'])) {
                 $params['date'] = Carbon::parse($_GET['date'])->format('Y-m-d');
             }
-
             $feedback = Feedback::get($params);
-            $this->view('/admin/feedback/index', $feedback, 'Admin');
+            $data = [
+                'feedback' => $feedback,
+                'room' => Room::get()
+            ];
+            $this->view('/admin/feedback/index', $data, 'Admin');
         } catch (CustomException $e) {
             ResponseHandler::setResponse($e->getMessage(), 'error');
             header('location:' . URL . '/admin/feedback/index');
