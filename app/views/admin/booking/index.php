71cc6f69c6dc86b8751652d6ab7c72f0f7e30d5e
Nugroho <nugrohonug066@gmail.com>
2025-12-10 12:45:40 +0700

    benerin bug bulan

--- app/views/admin/booking/index.php
+++ app/views/admin/booking/index.php
@@ -25,6 +25,22 @@ $statusLabel = [
     "finished" => "selesai"
 ];
 
+$bulan = [
+    '' => 'Semua',
+    '01' => 'Januari',
+    '02' => 'Februari',
+    '03' => 'Maret',
+    '04' => 'April',
+    '05' => 'Mei',
+    '06' => 'Juni',
+    '07' => 'Juli',
+    '08' => 'Agustus',
+    '09' => 'September',
+    '10' => 'Oktober',
+    '11' => 'November',
+    '12' => 'Desember'
+];
+
 // Opsi Room
 $roomOption = ['' => 'semua'];
 foreach ($data['room'] as $r):
