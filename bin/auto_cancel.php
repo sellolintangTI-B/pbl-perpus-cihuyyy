4e920c992c92b3155b24397b90a79329ca506a93
lvciddreams <farrelmaahira104@gmail.com>
2025-12-12 20:02:57 +0700

    Revert "benerin bug bug"
    This reverts commit c35fd4f40eeb5ab89bfb2cdef9f5b506a9c363cb.

--- bin/auto_cancel.php
+++ bin/auto_cancel.php
@@ -3,9 +3,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use App\Models\Booking;
 use App\Models\BookingLog;
-use App\Models\User;
 use App\Utils\Mailer;
-use Carbon\Carbon;
 
 $logFile = __DIR__ . '/scheduler_log.txt';
 
@@ -19,41 +17,26 @@ try {
 
 try {
     $data = BookingLog::getLateBookings();
-    if ($data) {
-        $emails = [];
-        $userIds = [];
+    $bookingIds = array_map(function($item){
+        return $item->id;
+    }, $data);
 
-        $bookingIds = array_map(function ($item) use (&$emails, &$userIds) {
-            $emails[] = $item->email;
-            $userIds[] = $item->user_id;
-            return $item->id;
-        }, $data);
+    $emails = array_map(function($item){
+        return $item->email;
+    }, $data);
 
+    $cancel = BookingLog::bulkCancel($bookingIds, 'Telat 10 menit');
 
-        foreach($userIds as $id) {
-            $checkSuspendUser = User::checkUserSuspend($id);
-            $suspension = User::update($id, [
-                'suspend_count' => $checkSuspendUser->suspend_count + 1
-            ]);
-            
-            if ($suspension->suspend_count >= 3) {
-                $suspendUser = User::suspendAccount($id);
-            }
-        }
-
-        $cancel = BookingLog::bulkCancel($bookingIds, 'Telat 10 menit');
-
-        if ($cancel) {
-            foreach ($emails as $email) {
-                Mailer::send($email, 'PEMBERITAHUAN', 'BOOKING ANDA TELAH DI CANCEL KARNA TELAT 10 MENIT DARI JADWAL YANG SUDAH ANDA BOOKING');
-            }
+    if($cancel) {
+        foreach($emails as $email) {
+            Mailer::send($email, 'PEMBERITAHUAN', 'BOOKING ANDA TELAH DI CANCEL KARNA TELAT 10 MENIT DARI JADWAL YANG SUDAH ANDA BOOKING');
         }
     }
-
-
+    
     $timestamp = date('Y-m-d H:i:s');
     $msg = "[$timestamp] SUKSES: Auto Cancel dijalankan.\n";
     file_put_contents($logFile, $msg, FILE_APPEND);
+
 } catch (Throwable $e) {
     $errorMsg = "[" . date('Y-m-d H:i:s') . "] ERROR: " . $e->getMessage() . "\n";
     file_put_contents($logFile, $errorMsg, FILE_APPEND);
